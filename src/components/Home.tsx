06031319e5e0d9d0bf85bc245da8572ff6b9a9a3
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-06-09 17:04:17 -0300

    todo list

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,19 +1,68 @@
 import { PlusCircle, Trash } from "phosphor-react";
+import { ChangeEvent, FormEvent, useState } from "react";
 
 import styles from './Home.module.css'
-
+interface Task {
+  id: number;
+  title: string;
+  isComplete: boolean;
+}
 export function Home() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [newTaskTitle, setNewTaskTitle] = useState('');
+  const [isComplete, setIsComplete] = useState(false);
+
+  function handleCreateNewTask(event: FormEvent) {
+    event.preventDefault();
+    if (!newTaskTitle) {
+      alert('Informe uma tarefa')
+      return;
+    }
+
+    const addTask = {
+      id: Math.floor(Math.random() * 100),
+      title: newTaskTitle,
+      isComplete
+    }
+
+    setTasks(oldState => [...oldState, addTask]);
+    setNewTaskTitle('');
+
+  }
+
+  function handleToggleTaskCompletion(id: number) {
+
+    const endTasks = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+    console.log(endTasks);
+    setTasks(endTasks)
+
+  }
+
+  function handleRemoveTask(id: number) {
+    // Remova uma task da listagem pelo ID   
+    const filterTask = tasks.filter(task => task.id != id)
+
+    setTasks(filterTask)
+
+  }
 
   return (
     <>
       <div className={styles.body}>
         <div className={styles.container}>
-          <form className={styles.form}>
-            <input type="text" placeholder='Adicione uma nova tarefa' />
-            <button>
+          <div className={styles.form}>
+            <input type="text"
+              placeholder='Adicione uma nova tarefa'
+              onChange={(e) => setNewTaskTitle(e.target.value)}
+              value={newTaskTitle}
+            />
+            <button type="submit" onClick={handleCreateNewTask}>
               Criar <PlusCircle size={16} />
             </button>
-          </form>
+          </div>
 
           <div className={styles.listTask}>
             <header>
@@ -26,29 +75,32 @@ export function Home() {
             </header>
 
             <ul>
-              <li className={styles.taskItem}>
-                <input type='checkbox' className={styles.taskCheckbox} />
-                <p className={styles.isActive}>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
-                <button title='Remover task' className={styles.taskRemove}>
-                  <Trash size={16} />
-                </button>
-              </li>
+              {tasks.map(task => (
+                <li className={styles.taskItem} key={task.id}
 
-              <li className={styles.taskItem}>
-                <input type='checkbox' className={styles.taskCheckbox} />
-                <p className={styles.isNotActive}>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
-                <button title='Remover task' className={styles.taskRemove}>
-                  <Trash size={16} />
-                </button>
-              </li>
+                >
 
-              <li className={styles.taskItem}>
-                <input type='checkbox' className={styles.taskCheckbox} />
-                <p className={styles.isActive}>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
-                <button title='Remover task' className={styles.taskRemove}>
-                  <Trash size={16} />
-                </button>
-              </li>
+                  <p
+                    className={task.isComplete ? styles.isActive : styles.taskCheckbox}
+                    onClick={() => handleToggleTaskCompletion(task.id)}
+                  >
+                    <input
+                      type="checkbox"
+
+                      readOnly
+                      checked={task.isComplete}
+
+                    />
+
+                    {task.title}
+
+                  </p>
+                  <button title='Remover task' className={styles.taskRemove} onClick={() => handleRemoveTask(task.id)}>
+                    <Trash size={16} />
+                  </button>
+                </li>
+              ))}
+
             </ul>
           </div>
 
